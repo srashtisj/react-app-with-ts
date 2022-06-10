6d06c9315372931e3e53c4c3c08fe8ea9dbd6c90
Srashti Jain <srashti0204@gmail.com>
2022-06-10 15:59:32 +0530

    Added Handler

--- src/components/Person/PersonDetail.tsx
+++ src/components/Person/PersonDetail.tsx
@@ -2,9 +2,14 @@ import "./PersonDetail.css";
 import { Card } from "../UI/Card";
 
 export const PersonDetail = (props: any) => {
+  const clickHandler = () => {
+    console.log("Clicked!");
+  };
+
   return (
     <Card className="person-detail-wrapper">
       <p>Name: {props.name}</p>
+      <button onClick={clickHandler}> Change </button>
     </Card>
   );
 };
