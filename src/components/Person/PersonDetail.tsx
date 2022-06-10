c71e246d22c067f73a9b9f734697cad06793e5b9
Srashti Jain <srashti0204@gmail.com>
2022-06-10 16:15:05 +0530

    Added useState

--- src/components/Person/PersonDetail.tsx
+++ src/components/Person/PersonDetail.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./PersonDetail.css";
 import { Card } from "../UI/Card";
 
 export const PersonDetail = (props: any) => {
+  const [name, setName] = useState(props.name);
+
+  //let title = props.name;
+
   const clickHandler = () => {
     console.log("Clicked!");
+    //title = "Updated";
+    setName("Updated!");
   };
 
   return (
     <Card className="person-detail-wrapper">
-      <p>Name: {props.name}</p>
+      <p>Name: {name}</p>
       <button onClick={clickHandler}> Change </button>
     </Card>
   );
