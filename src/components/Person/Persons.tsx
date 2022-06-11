495c874d20815a4b98ee336cae596e2324c938d2
Srashti Jain <srashti0204@gmail.com>
2022-06-11 14:14:02 +0530

    Conditional content

--- src/components/Person/Persons.tsx
+++ src/components/Person/Persons.tsx
@@ -2,12 +2,45 @@ import { Card } from "../UI/Card";
 import { PersonDetail } from "./PersonDetail";
 
 export const Persons = (props: any) => {
+  let personContent = <p> No person found</p>;
+  if (props.items.length > 0) {
+    personContent = props.items.map((item: any) => (
+      <PersonDetail key={item.name} name={item.name} />
+    ));
+  }
+
   return (
     <Card>
-      {props.items.map((item: any) => (
-        <PersonDetail key={item.name} name={item.name} />
-      ))}
+         {/** Type 5 */}
+      {personContent}
 
+
+      {/** Type 4 */}
+      {/* {props.items.length === 0 && <p> No person found</p>}
+
+      {props.items.length > 0 &&
+        props.items.map((item: any) => (
+          <PersonDetail key={item.name} name={item.name} />
+        ))} */}
+
+
+     { /** Type 3 */}
+      {/* {props.items.length === 0 ? (
+        <p> No person found</p>
+      ) : (
+        props.items.map((item: any) => (
+          <PersonDetail key={item.name} name={item.name} />
+        ))
+      )} */}
+
+
+     { /** Type 2 */}
+      {/* {props.items.map((item: any) => (
+        <PersonDetail key={item.name} name={item.name} />
+      ))} */}
+
+      
+     { /** Type 1 */}
       {/* <PersonDetail name={props.items[0].name} />
       <PersonDetail name={props.items[1].name} /> */}
     </Card>
